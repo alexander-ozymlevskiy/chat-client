7696b067b91336396200e041796a8f6330a0f13b
Alex <ozymlevskiy@Alexs-MacBook-Air.local>
2024-06-09 20:19:40 +0300

    origin

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -11,8 +11,6 @@ import Messages from "./Messages";
 
 const socket = io.connect("https://online-chat-igwx.onrender.com");
 
-console.log()
-
 const Chat = () => {
   const { search } = useLocation();
   const navigate = useNavigate();
